9042180febe5dc55da4515c68000923c6278774b
James Sturtevant <jstur@microsoft.com>
2020-09-28 16:26:37 -0700

    add attribuition

--- sysprep.ps1
+++ sysprep.ps1
@@ -1,3 +1,4 @@
+#modified from https://docs.microsoft.com/en-us/azure/virtual-machines/linux/image-builder-troubleshoot#sysprep-command-windows
 Write-Output '>>> Waiting for GA Service (RdAgent) to start ...'
 while ((Get-Service RdAgent).Status -ne 'Running') { Start-Sleep -s 5 }
 Write-Output '>>> Waiting for GA Service (WindowsAzureTelemetryService) to start ...'
